d0b93399faaef5417f7815547a556e92b5308465
Samuel Castillo <samuel.castillo@thalesgroup.com>
2022-03-14 19:35:46 +0100

    Hide game after page load and game over

--- client/src/app/game/game.component.ts
+++ client/src/app/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 import { Card, cardColors, GameLogic, isColorful, Player } from './types';
 
 @Component({
@@ -7,16 +8,11 @@ import { Card, cardColors, GameLogic, isColorful, Player } from './types';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.sass']
 })
-export class GameComponent implements OnInit {
+export class GameComponent {
 
   logic?: GameLogic;
 
   constructor(private toastr: ToastrService) { }
-
-  ngOnInit(): void {
-    this.logic = new GameLogic(['Lucas', 'George', 'Hayden', 'Ewan']);
-    this.logic.error$.subscribe(e => e && this.toastr.error(e.message))
-  }
 
   getCardBackgroundClass(player: Player, card: Card): string {
     if (player === this.logic?.activePlayer) {
@@ -25,6 +21,30 @@ export class GameComponent implements OnInit {
     return card.color;
   }
 
+  getCardDescription(player: Player, card: Card): string {
+    if (player === this.logic?.activePlayer) {
+      return 'Card';
+    }
+    return `${card.color.replace(/^./, char => char.toUpperCase())} ${card.number}`;
+  }
+
   readonly isColorful = isColorful;
   readonly allSingleColors = cardColors;
+
+  private subscriptions: Subscription[] = [];
+
+  start(): void {
+    this.logic = new GameLogic(['Lucas', 'George', 'Hayden', 'Ewan']);
+    this.subscriptions = [
+      this.logic.error$.subscribe(e => e && this.toastr.error(e.message)),
+      this.logic.gameOver$.subscribe(e => e && this.end())
+    ];
+  }
+
+  end(): void {
+    for (const sub of this.subscriptions) {
+      sub.unsubscribe();
+    }
+    this.logic = undefined;
+  }
 }
